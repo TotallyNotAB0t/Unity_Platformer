99a993e816f3f7704db2f611885b062b435e1be5
TotallyNotAB0t <pierre.durand95360@gmail.com>
2021-06-05 17:54:16 +0200

    second commit

---
+++ Assets/Controller/Controller.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    public float jumpForce;
+    private Rigidbody2D body;
+    private bool facingRight = true;
+    private bool isJumping = false;
+    public Animator animator;
+    private Vector3 start = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        Restart();
+    }
+
+    //Permet de deplacer le personnage et de le faire courir
+    private void Move()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+        animator.SetInteger("movementSpeed", (int)Input.GetAxisRaw("Horizontal"));
+
+    }
+
+    //Fait sauter le personnage et permet a l'animator de jouer le sprite correspondant
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(body.velocity.y) < 0.0001f)
+        {
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        if (Mathf.Abs(body.velocity.y) > 0.0001f)
+        {
+            isJumping = true;
+        }
+
+        animator.SetBool("isJumping", isJumping);
+        isJumping = false;
+    }
+
+    //Check le cote ou se situe le personnage et le fait tourner si besoin
+    private void Turn()
+    {
+        if ((Input.GetAxis("Horizontal") < 0) && facingRight)
+        {
+            transform.Rotate(0, -180, 0);
+            facingRight = false;
+
+        }
+        else if ((Input.GetAxis("Horizontal") > 0) && !facingRight)
+        {
+            transform.Rotate(0, -180, 0);
+            facingRight = true;
+        }
+    }
+
+    //Teleporte le joueur a "nouvellePosition"
+    private void changePosition(Vector3 newPosition)
+    {
+        transform.position = newPosition;
+    }
+
+    //Reset la position du personnage a 0
+    private void Restart()
+    {
+        animator.Play("blueRestart");
+        changePosition(start);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.collider);
+        //Si on rentre en contact avec une scie
+        if (col.collider.name == "Saw")
+        {
+            Restart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        Turn();
+
+        Jump();
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Unity_Platformer
+
+## First update
+
+Ongoing 2D platformer made in Unity
+
+It is using the Pixel Adventure assets by Pixel Frog : https://pixelfrog-assets.itch.io/pixel-adventure-1
+
+Everything else was made from scratch.
+Here is the current working features :
+- Movement of the player on the X & Y axis
+- Jump movement
+- Sprite facing the direction you are moving
+- Saw that kills the player
+- Restart mechanic when touching the saw
+- Every movement animated, except death.
+- Small Menu with 2 buttons : one to switch to the game scene, the other to quit the app
+
+Features to be implemented :
+- Add a real death (and don't allow the player to move)
+- Add checkpoints
+- Add ennemies + their scripted movement
+- Add moving platforms
+- Maybe add a walljump feature
+- More to come :)
+
+Thanks for passing by. Feel free to correct my bad code 💻 any time !
