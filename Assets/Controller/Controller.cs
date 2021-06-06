86b6eb70b0a21276bf9d8bd9b5ac66da7e4a3a26
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2021-06-06 22:12:30 +0200

    Cannot move during death
    Also deleted an unused death animation

--- Assets/Controller/Controller.cs
+++ Assets/Controller/Controller.cs
@@ -11,6 +11,7 @@ public class Controller : MonoBehaviour
     private bool isJumping = false;
     public Animator animator;
     private Vector3 checkpoint = new Vector3(0, 0, 0);
+    private bool canMove = true;
     //private int Lives = 3;
 
     // Start is called before the first frame update
@@ -46,10 +47,23 @@ public class Controller : MonoBehaviour
         isJumping = false;
     }
 
-    //Joue l'animation de mort
+    //Joue l'animation de mort et enclenche la fonction Restart (event de l'animator)
     private void Die()
     {
-        animator.Play("BlueDie");
+        animator.Play("blueDie");
+        NotMove();
+    }
+
+    //body.constraints not working on X axis so I just cooked my function
+    private void NotMove()
+    {
+        canMove = false;
+    }
+
+    //Same as above
+    private void MoveAgain()
+    {
+        canMove = true;
     }
 
     //Check le cote ou se situe le personnage et le fait tourner si besoin
@@ -79,6 +93,7 @@ public class Controller : MonoBehaviour
     {
         animator.Play("blueRestart");
         changePosition(checkpoint);
+        MoveAgain();
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -100,10 +115,14 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if (canMove)
+        {
+            Move();
 
-        Turn();
+            Turn();
 
-        Jump();
+            Jump();
+        }
+
     }
 }
